e255824145ccf1e208f476a2925c183b8a3e678f
Gil Wagner Oliveira <gillgner@hotmail.com>
2023-11-13 14:44:50 -0300

    Adicionado funcao de coleta de dados

---
+++ main.py
@@ -0,0 +1,17 @@
+def gether_data():
+    n1 = int(input("Primeiro valor: "))
+    n2 = int(input("Segundo valor: "))
+
+    returno n1, n2
+
+
+def main():
+    n1, n2 = gether_data()
+
+    print(n1*n2)
+    
+    return None
+
+
+if __name__ == "__main__":
+    main()
