9e9e5fde25b15191d3fd83308e6fd0b0d20dd07d
Gil Wagner Oliveira <gillgner@hotmail.com>
2023-11-15 23:22:44 -0300

    Criado funcao de exibicao de mensagem

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ def gether_data():
     n1 = int(input("Primeiro valor: "))
     n2 = int(input("Segundo valor: "))
 
-    returno n1, n2
+    return n1, n2
+
+def print_message(n1, n2):
+    print(f'Os valores {n1} e {n2} somados dão {n1+n2}')
+
+    return None
 
 
 def main():
     n1, n2 = gether_data()
 
-    print(n1**n2)
+    print_message(n1, n2)
 
     return None
 
